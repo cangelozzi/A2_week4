96ae9ecff3911da2fd2cbb393336ae442e8469de
camgelozzi <cangelozzi@gmail.com>
2018-09-26 11:48:53 -0400

    gulpfile created, and tested

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
 
+// Compile Sass
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
   .pipe(sass({outputStyle : 'compressed'}).on('error', sass.logError))
