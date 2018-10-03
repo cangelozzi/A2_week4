2f10461dec47688a77e181f23ae593a41116e947
camgelozzi <cangelozzi@gmail.com>
2018-10-03 09:42:43 -0400

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,22 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
 
-// Compile Sass
+// compile Sass task
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
   .pipe(sass({outputStyle : 'compressed'}).on('error', sass.logError))
   .pipe(gulp.dest('./css'));
 });
 
+// Optimize Images
+gulp.task('imageMin', () =>
+  gulp.src('images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('images'))
+);
+
+// watch task running 
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('images/*', ['imageMin']);
+});
